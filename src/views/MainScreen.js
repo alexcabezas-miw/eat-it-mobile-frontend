925e0028fbcd36890729f08ba133563dbe2648e8
Alejandro Cabezas Garríguez <alejandro.cabezas@alumnos.upm.es>
2022-06-05 11:11:17 +0200

    Add search and scan tabs

--- src/views/MainScreen.js
+++ src/views/MainScreen.js
@@ -2,7 +2,10 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ProfileScreen from "./profile/ProfileScreen";
-import ProductScreen from "./products/ProductScreen";
+import SearchScreen from "./products/SearchScreen";
+import ScanScreen from "./products/ScanScreen";
+
+
 import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
@@ -16,6 +19,18 @@ export default function MainScreen() {
                     fontSize: 17,
                 },
             })}>
+                <Tab.Screen name="Scan" component={ScanScreen} options={{
+                    tabBarLabel: 'Scan',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="scan" color={color} size={size} />
+                    ),
+                }} />
+                <Tab.Screen name="Buscar" component={SearchScreen} options={{
+                    tabBarLabel: 'Buscar',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="search" color={color} size={size} />
+                    ),
+                }} />
                 <Tab.Screen name="Mi perfil" component={ProfileScreen} options={{
                     tabBarLabel: 'Mi perfil',
                     tabBarIcon: ({ color, size }) => (
