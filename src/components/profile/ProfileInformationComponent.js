76c3ae8cab87767e51c16a163da3c338d7f5683e
Alex Cabezas <alexcabezas0705@gmail.com>
2022-06-20 02:47:38 +0200

    Add user restrictions information in profile

--- src/components/profile/ProfileInformationComponent.js
+++ src/components/profile/ProfileInformationComponent.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import ProfileGeneralInformationComponent from './ProfileGeneralInformationComponent'
+import ProfileIngredientsInformationComponent from './ProfileIngredientsInformationComponent'
+import ProfileRestrictionsInformationComponent from './ProfileRestrictionsInformationComponent'
 
 class ProfileInformationComponent extends Component {
     render() {
-        // Display the general information and the one related to intolerances, etc
         return (
             <View>
                 <ProfileGeneralInformationComponent {...this.props} />
+                <View style={{borderWidth: 1, borderColor: '#d0d0d0', borderStyle:'dashed'}}/>
+                <ProfileIngredientsInformationComponent {...this.props} />
+                <View style={{borderWidth: 1, borderColor: '#d0d0d0', borderStyle:'dashed'}}/>
+                <ProfileRestrictionsInformationComponent {...this.props} />
             </View>
         )
     }
